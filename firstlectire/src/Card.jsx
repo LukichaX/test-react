f3a46e89a4344345fb16f67e0113bc9e4dd41e9a
LukichaX <xirdaevi77@gmail.com>
2025-05-04 12:58:53 +0400

    first changes

---
+++ firstlectire/src/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <img
+        src="https://bcdn.alfuhod.com/media/catalog/product/cache/6e1b18c962905f3060c591e2c7ca3fb6/image/25907637a/astro-a50-x-lightspeed-wireless-gaming-headset-white.webp"
+        alt=""
+        className="card-pic"
+      />
+      <p className="title">Astro X50</p>
+      <p>349$</p>
+    </div>
+  );
+}
