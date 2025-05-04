a70edd55544eb97a9ea3c202f8cc81aed503aa6d
LukichaX <xirdaevi77@gmail.com>
2025-05-04 14:41:54 +0400

    done

--- firstlectire/src/App.jsx
+++ firstlectire/src/App.jsx
@@ -1,10 +1,40 @@
 import "./app.css";
 import Card from "./Card";
 
+const movies = [
+  {
+    title: "garry poder",
+    length: "2:30",
+    rating: 9.9,
+    img: "https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/uor-wwohp-logo-3-kids-clouds-key-art-hero-b.jpg",
+  },
+  {
+    title: "pacific rim",
+    length: "3:110",
+    rating: 9.9,
+    img: "https://m.media-amazon.com/images/M/MV5BMjI3Nzg0MTM5NF5BMl5BanBnXkFtZTgwOTE2MTgwNTM@._V1_.jpg",
+  },
+  {
+    title: "The Gorge",
+    length: "1:57",
+    rating: 8.8,
+    img: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQVhs0zWJkL9afwN2gqJJ3vtmX4yJVAeZchddbahTVkHzCi9fyKtXwj0OEcznWUsFRn_VKd",
+  },
+];
+
 function App() {
   return (
     <>
-      <Card />
+      {movies
+        .map((movie, i) => (
+          <Card
+            key={i}
+            title={movie.title}
+            length={movie.length}
+            rating={movie.rating}
+            img={movie.img}
+          />
+        ))}
     </>
   );
 }
--- firstlectire/src/Card.jsx
+++ firstlectire/src/Card.jsx
@@ -1,13 +1,15 @@
-export default function Card() {
+export default function Card(props) {
+  function like(title) {
+    console.log("liked ", { title });
+  }
+
   return (
     <div className="card">
-      <img
-        src="https://bcdn.alfuhod.com/media/catalog/product/cache/6e1b18c962905f3060c591e2c7ca3fb6/image/25907637a/astro-a50-x-lightspeed-wireless-gaming-headset-white.webp"
-        alt=""
-        className="card-pic"
-      />
-      <p className="title">Astro 50X</p>
-      <p>349$</p>
+      <img src={props.img} alt="" className="card-pic" />
+      <p className="title">{props.title.toUpperCase()}</p>
+      <p>length: {props.length}</p>
+      <p>rating {props.rating}/10 </p>
+      <button onClick={() => like(props.title)}>Like</button>
     </div>
   );
 }
